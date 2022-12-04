e806e4ae0eafbc55ebc7a31f92e2683729f5cdb3
Dibanana <chaunceygriffin3@gmail.com>
2022-12-04 01:17:07 -0800

    Can pick things up now
    The function to press E to pick items up now works. However, I am dumb and know that my method was more roundabout than it really should've been. (But it works, so tomorrow's problems will remain tomorrow's problems.)

--- My Moments Hero/Assets/Scripts/Items Scripts/Items/Weapons (If it's safe to do it)/UntouchedWeaponDrop.cs
+++ My Moments Hero/Assets/Scripts/Items Scripts/Items/Weapons (If it's safe to do it)/UntouchedWeaponDrop.cs
@@ -9,18 +9,29 @@ public class UntouchedWeaponDrop : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI PickUpText;
-
     //Use this for initialization - - Turns off text before arriving in range.
+
+    public ItemObject item; //lets all other codes know that the object attatched to this line of code is an item
+    public InventoryObject inventory; //allows the script to access inventory
+    private bool PickUpAllowed = false;
+
     private void Start(){
         PickUpText.gameObject.SetActive(false);
     }
     // Update is called once per frame
-
+    private void Update()
+    {
+        if (PickUpAllowed ==true && Input.GetKeyDown(KeyCode.E))
+        {
+            Pickup();
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.name.Equals("Player"))
         {
             PickUpText.gameObject.SetActive(true);
+            PickUpAllowed = true;
         }
     }
 
@@ -29,6 +40,13 @@ public class UntouchedWeaponDrop : MonoBehaviour
         if (collision.gameObject.name.Equals("Player"))
         {
             PickUpText.gameObject.SetActive(false);
+            PickUpAllowed = false;
         }
+    }
+    void Pickup()
+    {
+        var item=this.GetComponent<Item>();
+        inventory.AddItem(item.item); //adds the item to inventory. (hopefully it's properly defined.)
+        Destroy(gameObject);
     }
 }
--- My Moments Hero/Assets/Scripts/Player Scripts/Inventory.cs
+++ My Moments Hero/Assets/Scripts/Player Scripts/Inventory.cs
@@ -4,25 +4,10 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public InventoryObject inventory;
     public bool[] IsEquipped;
-    //private bool PickUpAllowed = false;
-
+    public InventoryObject inventory; //allows the script to access inventory
     // Update is called once per frame
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        var item = other.GetComponent<Item>();
-        if (item)
-        {
-            inventory.AddItem(item.item); //adds the item to inventory.
-            Destroy(other.gameObject);
-            StartCoroutine(Cooldown());  //This part is most likely irrelevant, but I added it and the enumerator cooldown myself to avoid duplicate pickup glitches. If anyone looks at this later, feel free to delete.
-        }
-    }
-    IEnumerator Cooldown()
-    {
-        yield return new WaitForSeconds(1f);
-    }
+
     private void OnApplicationQuit()
     {
         inventory.Container.Clear();
