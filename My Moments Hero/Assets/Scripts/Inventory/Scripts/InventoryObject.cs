91a2bd2dc2fe426a11cf1df16c758c5728d18f1d
Dibanana <chaunceygriffin3@gmail.com>
2022-12-04 03:26:44 -0800

    Added Save and Load Functions
    Press "P" to save
    Press "O" to load
    Inventory open/load conditionals are currently in inventory.cs
    I can add them to any object or button as well, but that will be a matter for later.
    
    Next video:
    https://www.youtube.com/watch?v=LcizwQ7ogGA&list=PLJWSdH2kAe_Ij7d7ZFR2NIW8QCJE74CyT&index=3

--- My Moments Hero/Assets/Scripts/Inventory/Scripts/InventoryObject.cs
+++ My Moments Hero/Assets/Scripts/Inventory/Scripts/InventoryObject.cs
@@ -1,35 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
-public class InventoryObject : ScriptableObject
+public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
 {
-    public List<InventorySlot> Container = new List <InventorySlot>();
+    public string SavePath;
+    private ItemDatabaseObject Database;
+    public List<InventorySlot> Container = new List<InventorySlot>();
+    
+    private void OnEnable()
+    {
+#if UNITY_EDITOR
+        Database = (ItemDatabaseObject)AssetDatabase.LoadAssetAtPath("Assets/Resources/Database.asset", typeof(ItemDatabaseObject));
+#else
+        Database = Resources.Load<ItemDatabaseObject>("Database");
+#endif
+    }
+
     public void AddItem(ItemObject _item)
     {
-        bool hasItem = false;
         for (int i=0; i < Container.Count; i++)
         {
             if(Container[i].item == _item)
             {
-                hasItem = true;
-                break;
+                return;
             }
         }
-        if(!hasItem)
+        Container.Add(new InventorySlot(Database.GetId[_item], _item));
+    }
+
+    public void Save()
+    {
+        string SaveData = JsonUtility.ToJson(this,true);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(string.Concat(Application.persistentDataPath, SavePath));
+        bf.Serialize(file, SaveData);
+        file.Close();
+    }
+    public void Load()
+    {
+        if(File.Exists(string.Concat(Application.persistentDataPath, SavePath)))
         {
-            Container.Add(new InventorySlot(_item));
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file =File.Open(string.Concat(Application.persistentDataPath, SavePath), FileMode.Open);
+            JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), this);
+            file.Close();
         }
     }
+
+    public void OnAfterDeserialize()
+    {
+        //for (int i = 0; i < Container.Count; i++)
+            //Container[i].item = Database.GetItem[Container[i].ID];
+                             //This code is used to save the amount of the item held.
+    }
+    public void OnBeforeSerialize()
+    {}
 }
 
 [System.Serializable]
 public class InventorySlot
 {
+    public int ID;
     public ItemObject item;
-    public InventorySlot(ItemObject _item)
+    public InventorySlot(int _id, ItemObject _item)
     {
+        ID = _id;
         item = _item;
     }
 }
--- My Moments Hero/Assets/Scripts/Player Scripts/Inventory.cs
+++ My Moments Hero/Assets/Scripts/Player Scripts/Inventory.cs
@@ -7,6 +7,17 @@ public class Inventory : MonoBehaviour
     public bool[] IsEquipped;
     public InventoryObject inventory; //allows the script to access inventory
     // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            inventory.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            inventory.Load();
+        }
+    }
 
     private void OnApplicationQuit()
     {
